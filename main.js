2b0e5e9076b85e3f3a5308c2bbf27d039c7c2dc0
Nooruddin <160733113+Nooruddin23@users.noreply.github.com>
2024-03-09 10:41:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var alamzaib = "Hello Alamzaib, would you like to learn some Python today?";
+console.log(alamzaib);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let Name = "Noor";
+console.log(`Hello ${Name}, would you like to learn some Typescript today?`);
